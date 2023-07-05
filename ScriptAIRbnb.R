ba17cb72b223e9e76786fb5750722c2738e1cbd9
MDankloff <93326445+MDankloff@users.noreply.github.com>
2023-07-05 15:33:22 +0200

    Revised fraudlabel dataset R

---
+++ ScriptAIRbnb.R
@@ -0,0 +1,111 @@
+##### INIT #############
+
+setwd(dirname(rstudioapi::getSourceEditorContext()$path)) 
+
+if(!require('tidyverse')) install.packages('tidyverse') #if(!require('stringr')) 
+library(tidyverse)
+library (randomForest)
+library (ggplot2)
+install.packages('cowplot')
+library (cowplot)
+library(caTools)
+
+options(stringsAsFactors = FALSE)
+
+##### LOAD DATA #############
+data_orig <- read.csv('listings.csv') %>% as_tibble
+#data_orig <- data_orig %>% 
+#  mutate(last_review = last_review %>% as_date()) 
+
+data_orig %>% glimpse
+
+##### ADD FRAUD LABELS #############
+
+fraud_rate <- 0.1
+n_case <- data_orig %>% nrow
+
+fraud <- rep(1, (n_case*fraud_rate) %>% round)
+nonfraud <- rep(0, n_case - (fraud %>% length) ) 
+all_label <- c(fraud, nonfraud) %>% sample
+
+all_label %>% length
+
+data_w_fraud <- data_orig %>%
+  mutate(fraud_label = all_label)
+
+data_w_fraud %>% glimpse
+
+##### ADD BIAS IN FRAUD LABELS for neigbourhood #############
+
+### Separate subset of the data
+hood <- "Oostelijk Havengebied - Indische Buurt"
+
+data_in_hood <- data_w_fraud %>% filter(neighbourhood == hood) 
+data_not_in_hood <- data_w_fraud %>% filter(neighbourhood != hood) 
+
+
+### Make new fraud labels for one neighborhood
+fraud_rate_hood <- 0.4
+n_case <- data_in_hood %>% nrow
+
+fraud <- rep(1, (n_case*fraud_rate) %>% round)
+nonfraud <- rep(0, n_case - (fraud %>% length) ) 
+new_label <- c(fraud, nonfraud) %>% sample
+
+data_in_hood <- data_in_hood %>%
+  mutate(fraud_label = new_label)
+
+
+### Merge modified data subset with the unmodified one.
+
+data_w_bias <- bind_rows(data_in_hood, data_not_in_hood)
+data_w_bias %>% nrow
+
+
+##### EXPORT NEW DATA #############
+
+data_w_bias %>% write.csv('new_listings.csv', row.names = FALSE)
+
+#### REMOVE NA PREDICTORS ######
+data_w_bias = subset(data_w_bias, select = -c(neighbourhood_group, reviews_per_month) )
+data_w_bias = subset(data_w_bias, select = -c(reviews_per_month) )
+
+
+#### CONVERTING OUTCOME TO FACTOR (otherwise random forest doesn't work) ####
+#data_w_bias[data_w_bias$fraud_label == 0,]$fraud_label <- "No Fraudster"
+#data_w_bias[data_w_bias$fraud_label == 1,]$fraud_label <- "Fraudster"
+
+data_w_bias$fraud_label <- as.factor(data_w_bias$fraud_label)
+
+####Split data set into train and test set ###
+set.seed(1234)
+
+split <- sample.split(data_w_bias$fraud_label, SplitRatio = 2/3)
+trainset <- subset(data_w_bias, split == TRUE)
+testset <- subset(data_w_bias, split == FALSE)
+
+trainset$fraud_label <- as.factor(train$fraud_label)
+testset$fraud_label <- as.factor(test$fraud_label)
+
+##### FITTING RANDOM FOREST REGRESSION TO DATA SET ####
+set.seed(1234)
+regressor = randomForest(x = trainset[1:14],
+                         y = trainset$fraud_label,
+                         ntree = 100)
+
+####Check optimal number of variables at each internal node in the tree####
+oob.values <- vector(length=10)
+for (i in 1:10) {
+  temp.regressor <- randomForest(fraud_label ~ ., data=trainset, mtry=i, ntree = 10)
+  oob.values[i] <- temp.regressor$err.rate[nrow(temp.regressor$err.rate),1] 
+}
+oob.values
+
+
+##make predictions on TEST data
+pred_test <- predict(regressor, newdata = testset, type = "class")
+
+pred_test
+
+
+
